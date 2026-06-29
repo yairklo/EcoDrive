78ee1576886bb00a383ac8580197e43e18eeefee
yairklo <yair.klausner@mail.huji.ac.il>
2026-06-29 12:16:19 +0300

    chore(devops): prepare production playbooks and docker orchestration [TASK-6.1][TASK-6.2]

---
+++ server/ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'ecodrive-api',
+      script: 'npm',
+      args: 'start',
+      instances: 'max',
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+    },
+  ],
+};
